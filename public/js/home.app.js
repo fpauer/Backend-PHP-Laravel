b346e7b601f778573add6f27187e287170124593
Fernando Pauer <fpauer@gmail.com>
2016-04-12 19:46:30 -0300

    REST API - CRUD

---
+++ public/js/home.app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var app = {}; // create namespace for the app
+
+//--------------
+// Models
+//--------------
+
+//--------------
+// Collections
+//--------------
+
+
+//--------------
+// Views
+//--------------
+
+
+//--------------
+// Routers
+//--------------
+
+//--------------
+// Initializers
+//--------------
+
+//app.appView = new app.AppView(); 
